0c537966f5196af65641879cba79007de2b05054
Fabian5150 <fabian.strauch.04@web.de>
2022-03-21 23:02:28 +0100

    Stunde vom 08.03.22

---
+++ Stunde vom 08.03.22/code.js
@@ -0,0 +1,15 @@
+console.log("Hallo, mein Name ist Fabian.")
+var name = "Fabian"
+console.log(name)
+console.log("Hallo, "+name)
+name = "Alfonso"
+console.log("Hallo, "+name)
+
+var lieblingsFarbe = "blau"
+console.log("Hallo, mein Name ist " + name + " meine Lieblingsfarbe ist " + lieblingsFarbe)
+var hobby = "Gitarre spielen"
+var alter = "17"
+var nachName = "Strauch"
+console.log("Hallo, mein Name ist " + name + " " + nachName + 
+", ich bin " + alter + " Jahre alt, meine Lieblingsfarbe ist " + lieblingsFarbe +
+" und ich mag " + hobby + ".")
